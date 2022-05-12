0998d7c3a5ee12935cf5bd7644e1b494f0fbd1f9
David Kuc <david.kuc@unit4.com>
2022-05-12 14:31:05 +0200

    Added Teacher CRUD windows

---
+++ SchoolApp/Views/Teacher/TeacherMain.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SchoolApp.Views.Teacher
+{
+    /// <summary>
+    /// Interaction logic for TeacherMain.xaml
+    /// </summary>
+    public partial class TeacherMain : Window
+    {
+        public TeacherMain()
+        {
+            InitializeComponent();
+        }
+
+        private void TeacherList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
