e4b9c9fa571612c87a22c724a080da945b068816
David Kuc <david.kuc@unit4.com>
2022-05-18 15:49:45 +0200

    Added TeacherUpdateGroups page

---
+++ SchoolApp2/Views/Teacher/TeaAddGroups.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SchoolApp2.Views.Teacher
+{
+    /// <summary>
+    /// Interaction logic for TeaAddGroups.xaml
+    /// </summary>
+    public partial class TeaAddGroups : Page
+    {
+        public TeaAddGroups()
+        {
+            InitializeComponent();
+        }
+
+        private void Confirm_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoBack_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddGroups_Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddGroups_Del_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
